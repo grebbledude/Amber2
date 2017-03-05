a3da8a619d39196123dbd3aaa46cfef534c448fe
Pete Bennett <petebennett@petes-air>
2017-03-05 18:57:16 +0000

    Initial Commit

---
+++ AmberLightTests/AmberLightTests.swift
@@ -0,0 +1,36 @@
+//
+//  AmberLightTests.swift
+//  AmberLightTests
+//
+//  Created by Pete Bennett on 05/03/2017.
+//  Copyright © 2017 Pete Bennett. All rights reserved.
+//
+
+import XCTest
+@testable import AmberLight
+
+class AmberLightTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
